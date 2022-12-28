51673cfe2050023ccb555458a0a5aec38f3f013e
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-12-27 20:02:43 -0400

    req 01, 06

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,13 +1,21 @@
 import { Request, Response } from 'express';
 import ProductServiceClass from '../services/product.service';
 
+import { Product } from '../interfaces/product.interface';
+
 class ProductControllerClass {
   constructor(private productService = new ProductServiceClass()) {}
 
-  public getProducts = async (_req: Request, res: Response) => {
-    const products = await this.productService.getProducts();
+  public getProducts = async (_req: Request, res: Response): Promise<void> => {
+    const products: Product[] = await this.productService.getProducts();
     res.status(200).json(products);
   };
+
+  public addProduct = async (req: Request, res: Response): Promise<void> => {
+    const { name, amount } = req.body;
+    const product: Product = await this.productService.addProduct(name, amount);
+    res.status(201).json(product);
+  };  
 }
 
 export = ProductControllerClass;
